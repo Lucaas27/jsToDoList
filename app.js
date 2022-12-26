282ecfed18aecc1710fbd3fe14b247bc9aa3c358
Lucas Gomes <lucasggomees@gmail.com>
2022-12-26 23:47:30 +0000

    Initial CSS

---
+++ app.js
@@ -0,0 +1,10 @@
+// Selectors
+
+const todoInput = document.querySelector()
+const todoBtn = document.querySelector()
+const todoList = document.querySelector()
+
+//Event Listeners
+
+
+//Functions
