7a77459f0ade7bb6336f162d29d2c63b54fa8ea7
Lucas Gomes <lucasggomees@gmail.com>
2022-12-27 11:41:37 +0000

    Created add functionality to the todo list

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
 // Selectors
-
-const todoInput = document.querySelector()
-const todoBtn = document.querySelector()
-const todoList = document.querySelector()
+const todoBtn = document.querySelector('.todo-button');
+const todoInput = document.querySelector('.todo-input');
+const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
+todoBtn.addEventListener('click', addTodo);
+todoList.addEventListener('click', changeState);
 
 
 //Functions
+function addTodo(e) {
+    //Prevent form from reloading
+    try {
+        e.preventDefault();
+
+    } catch (error) {
+        console.error(error)
+        alert('An error occurred and I need to write some code to handle this!');
+    }
+    //Container for todo-task
+    const taskContainer = document.createElement('article');
+    taskContainer.classList.add('todo-task-container');
+    //Individual tasks
+    const newTask = document.createElement('li');
+    newTask.classList.add('todo-task');
+    newTask.innerText = todoInput.value;
+    taskContainer.appendChild(newTask);
+    // Check Button
+    const checkBtn = document.createElement('button');
+    checkBtn.classList.add('todo-check');
+    checkBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    taskContainer.appendChild(checkBtn);
+    //Delete button
+    const delBtn = document.createElement('button');
+    delBtn.classList.add('todo-del');
+    delBtn.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
+    taskContainer.appendChild(delBtn);
+
+    // Append articles to list
+    todoList.appendChild(taskContainer);
+    //clear input value
+    todoInput.value = '';
+}
+
+function changeState(e) {
+    const action = e.target;
+    // console.log(action)
+}
